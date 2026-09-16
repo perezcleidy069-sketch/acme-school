0726ec32caad85ffbe7de584276d7b90d8d956b0
perezcleidy069-sketch <perezcleidy069@gmail.com>
2026-09-16 16:16:33 -0600

    feat: impletar la conexion de base de datos con mysql2

--- acme/teacher/app.js
+++ acme/teacher/app.js
@@ -8,3 +8,23 @@ const rl=createInterFace({
   input:process.stdin,
   output:process.stdout
 });
+
+let dbConnection;
+
+async function main(){
+  try{
+    dbConnection = await createConnection({
+      host: 'localhost',
+      user: 'campus2023',
+      password: 'campus2023',
+      database: 'acme_school'
+    });
+
+    console.log('Conectado exitosamente a al base de datos acme_campus en MySQL.\n')
+    await showMenu();
+  }
+  catch(error){
+    console.log('Erro al conectarse a la base de datos o al inicial la app.', error)
+  }
+
+}
